408cb96fd2a2ed93fb4773fd6d0df9f2782e3005
unknown <Utkarsh Gupta>
2025-11-01 23:16:27 +0530

    Easy Problem solution

---
+++ Codeforces/Easy.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int x=0;
+    string statement;
+
+    for(int i=0; i<n; i++){
+        cin>>statement;
+
+        if(statement.find("++")!= string::npos) x++;
+        else x--;
+    }
+    cout<<x<<endl;
+
+
+
+}
