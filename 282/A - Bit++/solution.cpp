5a12d36a287cc3acd97a8293598c769835ad08ca
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-06 20:58:33 +0530

    Add Bit++ [A] from Codeforces

---
+++ 282/A - Bit++/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>using namespace std; int main(){    int n;    cin>>n;    int x=0;    string statement;    for(int i=0; i<n; i++){        cin>>statement;        if(statement.find("++")!= string::npos) x++;        else x--;    }    cout<<x<<endl;}
