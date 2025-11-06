98d091cda48136b8c559ce8159a01d9ee97634df
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-06 21:04:01 +0530

    Add Chewbaсca and Number [A] from Codeforces

---
+++ 514/A - Chewbaсca and Number/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<string.h>#include<bits/stdc++.h>using namespace std; int main(){    string n;    cin>>n;    for(int i=0; i<n.size(); i++){        char c = n[i];        if(i==0 and c>='5' and c<'9') n[i] = '9' - c + '0';        else if(i>0 and c>='5') n[i] = '9' - c + '0';    }    cout<<n;    return 0;}
