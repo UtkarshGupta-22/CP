17d16554e446c668a798d1bda114348c2985a642
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-08 04:28:21 +0530

    Add Bear and Big Brother [A] from Codeforces

---
+++ 791/A - Bear and Big Brother/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    int a,b;    cin>>a>>b;    int cnt=0;    while(a<=b){        a = a*3;        b = b*2;        cnt++;        if(a>b)break;     }    cout<<cnt;    return 0;}
