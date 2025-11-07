db10a74c9846915e2c271f57ae9dd668dd5ffc25
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-07 15:23:42 +0530

    Add Wrong Subtraction [A] from Codeforces

---
+++ 977/A - Wrong Subtraction/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    int n, k;    cin>>n>>k;    while(k--){        int dig = n%10;        if(dig!=0) n--;        if(dig==0) n = n/10;    }    cout<<n;    return 0; }
