63736021dc1a0aa5b3a26f97be7ff047050759ec
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-08 04:29:53 +0530

    Add Word Capitalization [A] from Codeforces

---
+++ 281/A - Word Capitalization/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    string s;    cin>>s;     if(s[0] >= 'a' && s[0]<='z'){        int i = s[0]- 'a';        s[0] = 'A' + i;    }    cout<<s;    return 0;}
