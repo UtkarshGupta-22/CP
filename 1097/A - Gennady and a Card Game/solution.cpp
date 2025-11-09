993a8c8d707b8e4240ea7eeec5c00ab2cf4b0cfe
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-09 18:43:07 +0530

    Add Gennady and a Card Game [A] from Codeforces

---
+++ 1097/A - Gennady and a Card Game/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    string s;    cin>>s;    string a,b,c,d,e;    cin>>a>>b>>c>>d>>e;    if(s[0]==a[0] || s[0] ==b[0]  || s[0]==c[0]  || s[0]==d[0] || s[0]==e[0]         || s[1]==a[1] || s[1]==b[1] || s[1]==c[1] || s[1]==d[1] || s[1]==e[1]){         cout<<"YES";    }    else cout<<"NO";    return 0;  }
