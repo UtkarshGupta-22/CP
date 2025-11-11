63febd4d8d5802c8d61547fd313680ea25958770
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-11 11:43:35 +0530

    Add Erasing Zeroes [A] from Codeforces

---
+++ 1303/A - Erasing Zeroes/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream> using namespace std; int main(){    int t;    cin>>t;     while(t--){        string s;        cin>>s;        int prev=-1;        int res=0;        for(int i=0; i<s.length(); i++){            if(s[i]=='1') {                if(prev!=-1) res += (i-prev-1);                prev=i;        }    }    cout<<res<<endl;    }     return 0;}
