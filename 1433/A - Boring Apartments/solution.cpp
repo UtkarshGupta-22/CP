c6f1dd1f17eac477612e5d0bd4561c90e370f4ea
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-11 08:48:13 +0530

    Add Boring Apartments [A] from Codeforces

---
+++ 1433/A - Boring Apartments/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>using namespace std; int main(){    int t;    cin>>t;    int n;    while(t--){        cin>>n;        int digit = n%10;        int res=0;        res = (digit-1)*10;        int num = digit;        int i=1;        while(num<=n){            res += (i++);            num = num*10+digit;        }        cout<<res<<endl;    }    return 0; }
