ccb0db078b227360b9ecbd18f14bdc5a6d8098b5
Utkarsh Gupta <utkarshg646@gmail.com>
2025-11-19 22:37:04 +0530

    Add Assigning to Classes [B] from Codeforces

---
+++ 1300/B - Assigning to Classes/solution.cpp
@@ -0,0 +1,1 @@
+#include<iostream>#include<vector>#include<algorithm>using namespace std; int main(){    int t;    cin>>t;    while(t--){        int n;        cin>>n;        vector<int>v(2*n);        for(int i=0; i<2*n; i++) cin>>v[i];        sort(v.begin(),v.end());        int res = abs(v[n] - v[n-1]);        cout<<res<<endl;    }    return 0;  }
